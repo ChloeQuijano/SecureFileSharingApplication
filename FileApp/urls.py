b6315c4d4a52806dbd1f3736547134f4f5d07f7f
ChloeQuijano <61916412+ChloeQuijano@users.noreply.github.com>
2023-11-03 20:16:10 -0400

    Started adding urls and views functions for the pages we need

--- FileApp/urls.py
+++ FileApp/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include 
+from . import views
 
 app_name = 'file_app'
-urlpatterns = [
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('upload_file/', views.upload_file, name='upload_file'),
 ]
--- FileApp/views.py
+++ FileApp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# TODO: Make home page where navigation bar contains sign up / login
+def home(request):
+    return HttpResponse("This is the home page")
+
+# TODO: Register a new user account page
+def register(request):
+    return HttpResponse("This is the register page")
+
+# TODO: Login page for user
+def login(request):
+    return HttpResponse("This is the login page")
+
+# TODO: Logout page for user
+def logout(request):
+    return HttpResponse("This is the logout page")
+
+# TODO: After login, can view the files for the user
+def profile(request):
+    return HttpResponse("This is the profile page")
+
+# TODO: Here you will be able to upload a file for that user
+def upload_file(request):
+    return HttpResponse("This is the upload file page")
