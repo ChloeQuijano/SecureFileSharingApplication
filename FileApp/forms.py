d26d7f5919c34b220c725a4f9f549268164744a9
Chloe Quijano <61916412+ChloeQuijano@users.noreply.github.com>
2023-11-04 23:16:21 -0400

    Update html for login/signup forms (#4)
    * update form html and start functions
    
    * update logout redirect

--- FileApp/forms.py
+++ FileApp/forms.py
@@ -1,5 +1,16 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50)
     file = forms.FileField()
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields = ['username','email','password1','password2']
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=65)
+    password = forms.CharField(max_length=65, widget=forms.PasswordInput) 
