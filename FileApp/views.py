0314ecee439711bb2d792e92013c693c8e9d0090
Chloe Quijano <61916412+ChloeQuijano@users.noreply.github.com>
2023-11-06 21:22:41 -0500

    Fix html for file upload page (#8)
    * fix html for file upload form
    
    * cleanup

--- FileApp/views.py
+++ FileApp/views.py
@@ -96,8 +96,6 @@ def sign_out(request):
 # Can view the files for the user
 @login_required
 def profile(request):
-    # FIXME: Need to get files ONLY for that user, if not logged in, will prompt user to login instead
-    # Fixed - if not logged in it will redirect them to the login page (LOGIN_URL added in settings.py) - M
     files = File.objects.filter(owner=request.user)
     # users = User.objects.exclude(id=request.user.id)  # Get all users except the current user
     context = {
@@ -112,8 +110,9 @@ def profile(request):
 def upload_file(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
-        #TODO: Handle checking file integrity here and create file object to save after checking
+        # Handle checking file integrity here and create file object to save after checking
         if form.is_valid():
+            # TODO: check file integrity here
             # TODO: need to make inputs correct for creating file object
             # file_instance = File(file=request.FILES["file"])
             # file_instance.save()
